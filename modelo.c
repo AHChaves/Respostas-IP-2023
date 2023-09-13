9ee6353c28740d9052ec438cde8ba4391dc70b84
Arthur Henrique <85091282+AHChaves@users.noreply.github.com>
2023-09-13 12:47:57 -0300

    ta tudo errado

--- modelo.c
+++ modelo.c
@@ -1,26 +1,86 @@
-#include <stdio.h>  // Biblioteca que exporta funcoes in e out
-#include <locale.h> //Biblioteca que exporta as linguagens
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-#define SUCESSO 0 // Define Sucesso
+#define STRING_SIZE (100+1)
+#define SUCESSO 0 
 
-void Questao1() {} // Roda a questao 1
+void Fatorial() // faz o fatorial de um numero
+{
+    int i, num, fatorial;
 
-void Questao2() {} // Roda a questao 2
+    printf("Digite um n√∫mero: ");
+    scanf("%d", &num);
 
-void Questao3() {} // Roda a questao 3
+    for (i = 1; i < num; i++)
+        fatorial = fatorial * i;
 
-void Questao4() {} // Roda a questao 4
+    printf("O fatorial de %d √© %d!\n", num, fatorial);
 
-void Questao5() {} // Roda a questao 5
+} 
 
-void Questao6() {} // Roda a questao 6
+void ListaDeCompras(){
 
-void Questao7() {} // Roda a questao 7
+    int quantidade;
+    char** item; //matriz de strings
+
+    printf("Informe quantos itens deseja que a lista tenha: ");
+    scanf("%d%*c", &quantidade); 
+
+    item = (char**)malloc(sizeof(char*) * quantidade); //atribui o tamanho do string
+
+    for(int i = 0; i < quantidade; i++){
+        item = (char**)malloc(sizeof(char*) * STRING_SIZE); // atribui o tamanho da string
+        printf("item %d: ", (i + 1));
+        fgets(item[i], STRING_SIZE, stdin);
+    }
+
+    for(int i = 0; i < quantidade; i++){ //mostra o vetor de string
+        printf("item n %d: %s", (i+1), item[i]);
+    }
+
+}
+
+void CadastraNotas(){
+
+    int numNotas, novoNumNotas, i;
+    int* notas;
+    bool valorValido = false;
+
+    printf("Quantas notas ser√£o distribuidas: ");
+    scanf("%d", &numNotas);
+
+    notas = (int*)malloc(sizeof(int) * numNotas); //atribui o tamanho do vetor de notas
+
+    printf("Cadastre as notas de um aluno: \n");
+
+    for(i = 0; i < numNotas; i++) { //atribui valor as notas
+        printf("nota %d: ", (i+1));
+        scanf("%d", &notas[i]);
+    }
+
+    do{
+        printf("defina quantas notas ser√£o cadastradas a mais(deve ser um valor maior do que o anterior): ");
+        scanf("%d", &novoNumNotas);
+        valorValido = novoNumNotas <= numNotas ? false : true; //operador ternario
+    }while(!valorValido);
+
+    notas = (int*)realloc(notas, sizeof(int) * novoNumNotas); //realoca o tamanho do vetor de notas
+
+    printf("As notas cadastradas foram: \n");
+
+    for(i = 0; i < (novoNumNotas - numNotas); i++) {
+        printf("nota %d: ", (i+1));
+        scanf("%d", &notas[i]);
+    }
+
+    for(i = 0; i <(novoNumNotas - numNotas); i++){ // imprime o vetor
+        printf("nota %d: %d\n", i, notas[i]);
+    }
+}
 
 int main(int argc, char **argv)
 {
-
-    setlocale(LC_ALL, ""); // atribui a linguagem para o codigo
 
     int escolha;
 
@@ -30,78 +90,39 @@ int main(int argc, char **argv)
         printf("================================\n");
 
         // Mostra as opcoes disponiveis
-        printf("Escolha uma das opÁıes:\n[1] Quest„o 1\n[2] Quest„o 2\n[3] Quest„o 3\n[4] Quest„o 4\n[5] Quest„o 5\n[6] Quest„o 6\n[7] Quest„o 7\n[0] Fechar programa\nEscolha:\t");
+        printf("0 - Sair\n1 - fatorial\n2 - Lista de compras\n3 - notas\nEscolha:\t");
         scanf("%d", &escolha);
 
         switch (escolha)
         {
-        case 0:
-            break;
-
+        case 0: break;
         case 1:
         {
-            printf("\e[1;1H\e[2J"); //printf que apaga o terminal
-
-            Questao1(); //Chama a questao 1
-
-            printf("\e[1;1H\e[2J");
+            Fatorial();
             break;
         }
         case 2:
         {
-            printf("\e[1;1H\e[2J");
-
-            Questao2();
-
-            printf("\e[1;1H\e[2J");
+            ListaDeCompras();
             break;
         }
         case 3:
         {
-            printf("\e[1;1H\e[2J");
-
-            Questao3();
-
-            printf("\e[1;1H\e[2J");
+            CadastraNotas();
             break;
         }
         case 4:
         {
-            printf("\e[1;1H\e[2J");
-
-            Questao4();
-
-            printf("\e[1;1H\e[2J");
             break;
         }
         case 5:
         {
-            printf("\e[1;1H\e[2J");
-
-            Questao5();
-
-            printf("\e[1;1H\e[2J");
-            break;
-        }
-        case 6:
-        {
-            printf("\e[1;1H\e[2J");
-            Questao6();
-            break;
-        }
-        case 7:
-        {
-            printf("\e[1;1H\e[2J");
-
-            Questao7();
-
-            printf("\e[1;1H\e[2J");
             break;
         }
         default:
         {
-            printf("\e[1;1H\e[2J");
-            printf("\nOpÁ„o invalida!!!\n");
+            printf("\nOpcao invalida!!!\n");
+            break;
         }
         }
 
